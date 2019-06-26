e86b6307cd11474b1bffcd605873bd8480c109a6
Ricardo Gimenes Ferreira <gimenesferreira@gmail.com>
2019-06-25 23:47:03 -0300

    first commit

--- sugest.php
+++ sugest.php
@@ -7,56 +7,35 @@ if (isset($_GET['q'])) {
   "response": {
     "deals": [
       {
-        "deal_id": "5c548cd1e4b09d529dfa6a22",
-        "page_id": "sao-paulo-so",
-        "short_title": "Rodízio de Pizza",
-        "formatted_url": "rodizio-de-pizzamija",
+        "page_id": "folder-id",
+        "short_title": "Title",
+        "formatted_url": "deal-url",
         "partner": {
-          "name": "Pizzaria Geovanele",
-          "formatted_url": "pizzaria-geovanele",
-          "canonical_url": ""
+          "name": "lorem ipsum",
+          "formatted_url": "partner-id"
         },
         "images": [
           {
             "image": "https://img.stpu.com.br/?img=https://s3.amazonaws.com/pu-mgr/default/a0R0f000010xIRvEAM/5c58283ce4b0f1fdfd052fcf.jpg&w=64&h=41"
           }
         ],
-        "page": [
-          "sao-paulo-alphaville",
-          "osasco",
-          "sao-paulo-so",
-          "barueri",
-          "sao-paulo"
-        ],
-        "dealOriginalImage": "",
         "dealImage": "https://img.stpu.com.br/?img=https://s3.amazonaws.com/pu-mgr/default/a0R0f000010xIRvEAM/5c58283ce4b0f1fdfd052fcf.jpg&w=64&h=41"
-      },
-      {
-        "deal_id": "57308711e4b05901a0ad6a1f",
-        "page_id": "sao-paulo-so",
-        "short_title": "Pizza Grande com opção de Pizza Doce",
-        "formatted_url": "pizza-salgada-com-pizza-doce",
+      },{
+        "page_id": "folder-id",
+        "short_title": "Title",
+        "formatted_url": "deal-url",
         "partner": {
-          "name": "Mescla Pizza Bar",
-          "formatted_url": "mescla-pizza-bar",
-          "canonical_url": ""
+          "name": "lorem ipsum",
+          "formatted_url": "partner-id"
         },
         "images": [
           {
-            "image": "https://img.stpu.com.br/?img=https://s3.amazonaws.com/pu-mgr/default/a0RG000000p5mtDMAQ/5ad49d9ee4b033ed7286bdb8.png&w=64&h=41"
+            "image": "https://img.stpu.com.br/?img=https://s3.amazonaws.com/pu-mgr/default/a0R0f000010xIRvEAM/5c58283ce4b0f1fdfd052fcf.jpg&w=64&h=41"
           }
         ],
-        "page": [
-          "sao-paulo-so",
-          "sao-paulo"
-        ],
-        "dealOriginalImage": "",
-        "dealImage": "https://img.stpu.com.br/?img=https://s3.amazonaws.com/pu-mgr/default/a0RG000000p5mtDMAQ/5ad49d9ee4b033ed7286bdb8.png&w=64&h=41"
+        "dealImage": "https://img.stpu.com.br/?img=https://s3.amazonaws.com/pu-mgr/default/a0R0f000010xIRvEAM/5c58283ce4b0f1fdfd052fcf.jpg&w=64&h=41"
       }
-    ],
-    "lastOffset": 3,
-    "hasMore": true,
-    "total": 111
+    ]
   }
 }
 	<?
@@ -67,35 +46,21 @@ if (isset($_GET['q'])) {
   "response": {
     "deals": [
       {
-        "deal_id": "5c548cd1e4b09d529dfa6a22",
-        "page_id": "sao-paulo-so",
-        "short_title": "Rodízio de Pizza",
-        "formatted_url": "rodizio-de-pizzamija",
+        "page_id": "folder-id",
+        "short_title": "Title",
+        "formatted_url": "deal-url",
         "partner": {
-          "name": "Pizzaria Geovanele",
-          "formatted_url": "pizzaria-geovanele",
-          "canonical_url": ""
+          "name": "lorem ipsum",
+          "formatted_url": "partner-id"
         },
         "images": [
           {
             "image": "https://img.stpu.com.br/?img=https://s3.amazonaws.com/pu-mgr/default/a0R0f000010xIRvEAM/5c58283ce4b0f1fdfd052fcf.jpg&w=64&h=41"
           }
         ],
-        "page": [
-          "sao-paulo-alphaville",
-          "osasco",
-          "sao-paulo-so",
-          "barueri",
-          "sao-paulo"
-        ],
-        "dealOriginalImage": "",
         "dealImage": "https://img.stpu.com.br/?img=https://s3.amazonaws.com/pu-mgr/default/a0R0f000010xIRvEAM/5c58283ce4b0f1fdfd052fcf.jpg&w=64&h=41"
-      }
-    ],
-    "lastOffset": 3,
-    "hasMore": true,
-    "total": 111
-  }
-}<?
+      }]
+    }
+  }<?
 }
 ?>
