6c0f6b5cf545a2304ef5f54481d3c11dbc7d813d
Ricardo Gimenes Ferreira <ricardogi@ricardogi-macbookpro.roam.corp.google.com>
2019-06-28 13:39:12 -0300

    created a simple search

--- sugest.php
+++ sugest.php
@@ -7,34 +7,32 @@ $json = file_get_contents('deals.json');
 $json_data = json_decode($json,true);
 
 if (isset($_GET['q'])) {
-  //var_dump($_GET['q']);
+  
   $q = $_GET['q'];
-  //var_dump($q);
+  
 	if ($q !='') {
 
 
 	  $deals = array_filter($json_data['response']['deals'], function($deal) {
       global $q;
       if( strpos( $deal['partner']['name'], $q) !== false  )
-
-        {return true; } 
-        else 
-        {return false; };
+        {return true; }
     });
-   // var_dump('');
-    //var_dump($deals);
+   
+    
     $json = array(
       'code' =>  200,
       "response" =>  array(
-          'deals' => array(reset($deals))
+          'deals' => array_values($deals)
         )
       );
-    echo json_encode($json, JSON_PRETTY_PRINT);
-    echo ($response);
+    
+    echo json_encode($json);
+
 	}else{
-    //echo json_encode($json_data);
+    
   }
 } else{
-  //echo json_encode($json_data);
+  
 }
 ?>
